7a1de827e16e4713dac96c97ea9e02f5f7c575a6
zasqiaandiiii <114038872+zasqiaandiiii@users.noreply.github.com>
2022-11-20 20:37:42 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+package com.dicoding.picodiploma.fragmentlayoutex_f55121007;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appBar_id);
+        viewPager = (ViewPager) findViewById ((R.id.Viewpager_id));
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new FragmentHome(), "Home");
+        adapter.AddFragment(new FragmentExplore(), "Explore");
+        adapter.AddFragment(new FragmentAccount(), "Account");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
